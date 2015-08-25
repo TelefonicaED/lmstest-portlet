c7fe02363f076030fe6d5b731bf5504b25f2e30e
erchiky <erchiky@gmail.com>
2015-08-25 09:44:05 +0200

    Subida desde google code a gitHub

--- docroot/WEB-INF/src/com/tls/liferaylms/main/Main.java
+++ docroot/WEB-INF/src/com/tls/liferaylms/main/Main.java
@@ -31,8 +31,7 @@ public class Main {
 		Context.setTeacherName(TestProperties.get("teacher-name")); //teacher
 		Context.setTeacherPass(TestProperties.get("teacher-pass")); //teacher
 		Context.setTeacherUser(TestProperties.get("teacher-email")); //teacher@liferay.com
-		
-		 
+				
 		Class[] classes = null;
 		try {
 			classes = ClassFinder.getClasses("com.tls.liferaylms.test.unit");
